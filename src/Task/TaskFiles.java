0772db3c78cccb5146216d3e369982ea150d9cf3
mr1er0y <leonid.salkov0002@gmail.com>
2022-04-29 00:30:43 +0300

    move TaskFiles.java

--- src/Task/TaskFiles.java
+++ src/Task/TaskFiles.java
@@ -1,3 +1,5 @@
+package Task;
+
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
